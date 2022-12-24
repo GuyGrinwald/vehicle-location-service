5045b3d82c6b9e03f47c840c1fc12735a2184ec3
Guy Grinwald <guy@grinwald.com>
2022-12-24 16:18:02 +0200

    Fixed logging issue

--- utils/logging_config.py
+++ utils/logging_config.py
@@ -1,4 +1,7 @@
 import logging
 import logging.config
+from os import path
 
-logging.config.fileConfig("logging.conf")
+
+log_file_path = path.join(path.dirname(path.abspath(__file__)), "logging.conf")
+logging.config.fileConfig(log_file_path)
